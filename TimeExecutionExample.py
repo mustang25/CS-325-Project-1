986256b24037473bb92899b00d9f5a4d26916b38
jhartle <blakeyjo@oregonstate.edu>
2016-10-11 19:45:02 -0600

    Example of timing execution of program in Python 3

---
+++ TimeExecutionExample.py
@@ -0,0 +1,30 @@
+# Calculate the running time of a program
+# Created by John Blakey
+# From Question 7 of HW1
+
+#Python module used to time execution
+import time
+
+# Example function to test running time
+def fibRec(n):
+	if n == 0:
+		return 0
+	elif n == 1:
+		return 1
+	else:
+		return fibRec(n - 1) + fibRec(n - 2)
+
+# Ask for input
+userIn = int(input('Input the number of Fibonacci numbers to calculate:'))
+
+# start clock
+start = time.clock()
+
+# run example function to time its execution
+print(fibRec(userIn))
+
+# end clock
+end = time.clock()
+
+# print clock results
+print(end - start)
