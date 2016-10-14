d9ca69949c54ad53456d9d1ad35abde5de36b4b6
Rob Navarro <robnavarro25@gmail.com>
2016-10-14 12:40:45 -0600

    Redid first enumeration function to reflect what assignment actually asked for. Originally I programmed it to what part two was asking for. Tested using arrays given in example.

--- enumeration.py
+++ enumeration.py
@@ -2,14 +2,32 @@
 
 
 def enumeration(array):
-    max_sum = 0
+    max_sum = array[0]
+    start_index = 0
+    end_index = 1
+
+    for i, first_value in enumerate(array):
+        for j, next_value in enumerate(array, i+1):
+            current_sum = 0
+            for value in array[i:j+1]:
+                current_sum += value
+            if current_sum > max_sum:
+                max_sum = current_sum
+                start_index = i
+                end_index = j+1
+
+    return array[start_index:end_index], max_sum
+
+
+def better_enumeration(array):
+    max_sum = array[0]
     start_index = 0
     end_index = 1
 
     for i, first_value in enumerate(array):
         current_sum = first_value
 
-        for j, next_value in enumerate(array[i+1:], i + 1):
+        for j, next_value in enumerate(array[i + 1:], i + 1):
             current_sum += next_value
 
             if current_sum > max_sum:
@@ -21,7 +39,7 @@ def enumeration(array):
 
 
 if __name__ == '__main__':
-    arr = [10, -11, -1, -9, 33, -45, 23, 24, -1, -7 -8, 19]
+    arr = [-12, -42, -11]
     return_vals = enumeration(arr)
 
     print("{}\n{}\n{}".format(arr, return_vals[0], return_vals[1]))
