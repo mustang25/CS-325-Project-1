aa94a4ce962e285b1c62ea4a896c449833caf356
Rob Navarro <robnavarro25@gmail.com>
2016-10-15 21:42:02 -0600

    Testing file for timing algorithm running times

---
+++ timing.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import random
+import time
+from algo2_better_enumeration import enumeration, better_enumeration
+from algo3_divide_and_conquer_time_test import divide_and_conquer
+from algo4_linear import linear
+
+text_output = open('timing.txt', 'w')
+
+# Algorithm 1 testing.
+total_time = 0
+n = [100, 200, 300, 350, 400, 450, 500, 550, 600, 650]
+text_output.write("Enumeration Results:\n")
+text_output.write("Array Size\tRunning Time\n")
+for size in n:
+    total_time = 0
+    for _ in range(10):
+        test_array = [random.randrange(-100, 100) for i in range(size)]
+        start_time = time.time()
+        enumeration(test_array)
+        execution_time = time.time() - start_time
+        total_time += execution_time
+
+    average = total_time/10
+
+    text_output.write("{}\t\t\t{}\n\n".format(size, average))
+
+# Algorithm 2 testing
+total_time = 0
+n = [100, 200, 300, 400, 500, 1000, 2000, 4000, 8000, 10000]
+text_output.write("Better Enumeration Results:\n")
+text_output.write("Array Size\tRunning Time\n")
+for size in n:
+    total_time = 0
+    for _ in range(10):
+        test_array = [random.randrange(-100, 100) for i in range(size)]
+        start_time = time.time()
+        better_enumeration(test_array)
+        execution_time = time.time() - start_time
+        total_time += execution_time
+
+    average = total_time/10
+
+    text_output.write("{}\t\t\t{}\n\n".format(size, average))
+
+# Algorithm 3 testing
+text_output.write("Divide & Conquer Results:\n")
+text_output.write("Array Size\tRunning Time\n")
+for size in n:
+    total_time = 0
+    for _ in range(10):
+        test_array = [random.randrange(-100, 100) for i in range(size)]
+        total_time += divide_and_conquer(test_array)
+
+    average = total_time/10
+
+    text_output.write("{}\t\t\t{}\n\n".format(size, average))
+
+# Algorithm 4 testing
+text_output.write("Linear Results:\n")
+text_output.write("Array Size\tRunning Time\n")
+for size in n:
+    total_time = 0
+    for _ in range(10):
+        test_array = [random.randrange(-100, 100) for i in range(size)]
+        start_time = time.time()
+        linear(test_array)
+        execution_time = time.time() - start_time
+        total_time += execution_time
+
+    average = total_time/10
+
+    text_output.write("{}\t\t\t{}\n\n".format(size, average))
+
+text_output.close()
